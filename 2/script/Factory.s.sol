efd2cf40d3efc1463f83cd3e77aa5146b68d8a10
renesuno <renesuno@gmail.com>
2025-09-30 18:57:56 +0900

    assignment-4 settings

--- 2/script/Factory.s.sol
+++ 2/script/Factory.s.sol
@@ -40,38 +40,35 @@ contract FactoryScript is Script {
 // ==========================
 
 // == Logs ==
-//   MiniAMMFactory deployed at: 0x49c5998a3d99B9d755906196219530D21C21fD6D
-//   TokenA deployed at: 0x3c723fa37a98CBE904EDC9B8C65b7D6CCD6b215F
-//   TokenB deployed at: 0xA909D7290bfFb010FFB81061094Cf7cF4Ca1BD57
-//   MiniAMM pair deployed at: 0x74ADde0f6Bc75199ccEA77B1A4c8389eb40aB868
-
-// ## Setting up 1 EVM.
-// ==========================
+//   MiniAMMFactory deployed at: 0x87D927A7c8dD7C99df8d5DB758512Dc7a7E667cD
+//   TokenA deployed at: 0x2203a23279483aA116C4b46Cd924171A1BC028f3
+//   TokenB deployed at: 0x3d2e6Bf44c8ba2DB5cC29A1536752d6F03E5e32F
+//   MiniAMM pair deployed at: 0xFf31A5e09143e9A8079214D1072f5F3a552f5540
 
 // ==========================
 
 // ##### flare-coston2
-// ✅  [Success] Hash: 0x237de628497d0eed384d949b482b00c155da8df123f8df5bb7dbc29fc437bbe9
-// Contract Address: 0x3c723fa37a98CBE904EDC9B8C65b7D6CCD6b215F
-// Block: 22437640
-// Paid: 0.0615736875 C2FLR (985179 gas * 62.5 gwei)
-
-// ##### flare-coston2
-// ✅  [Success] Hash: 0xc2bab0553e4e6e8aeb33a0ee0b6a1b0a7da7e315d38e1b1087319d61019d3403
-// Contract Address: 0x49c5998a3d99B9d755906196219530D21C21fD6D
-// Block: 22437640
+// ✅  [Success] Hash: 0x8b0b703a50aca94cba32e1972fd4e0e51619d302b23978fffa4125012f7926f1
+// Contract Address: 0x87D927A7c8dD7C99df8d5DB758512Dc7a7E667cD
+// Block: 22439515
 // Paid: 0.20060825 C2FLR (3209732 gas * 62.5 gwei)
 
 // ##### flare-coston2
-// ✅  [Success] Hash: 0xe8f670ba8ff298d1c91225d278cf79bc2fafba8bed8c2b580f79503e36871a0b
-// Contract Address: 0xA909D7290bfFb010FFB81061094Cf7cF4Ca1BD57
-// Block: 22437640
+// ✅  [Success] Hash: 0xcfcb9d2b1a2280027b642edc7174d163909ae8fe1a4591e0512c0a36061303d0
+// Block: 22439516
+// Paid: 0.1421888125 C2FLR (2275021 gas * 62.5 gwei)
+
+// ##### flare-coston2
+// ✅  [Success] Hash: 0xb779eaf197fcb4b63cdb38b3fe389d98b895fa8851827c5d48150caaf71bf27b
+// Contract Address: 0x2203a23279483aA116C4b46Cd924171A1BC028f3
+// Block: 22439516
 // Paid: 0.0615736875 C2FLR (985179 gas * 62.5 gwei)
 
 // ##### flare-coston2
-// ✅  [Success] Hash: 0x4a7edd10a0bdfb1a33f0d95aa871f61dcb28bf467450a5c9b02379375e40d159
-// Block: 22437640
-// Paid: 0.1421888125 C2FLR (2275021 gas * 62.5 gwei)
+// ✅  [Success] Hash: 0x8ea22323c76366c60c68e7fb3b1c17445b5c5c55040f46f697f1c79986647ff7
+// Contract Address: 0x3d2e6Bf44c8ba2DB5cC29A1536752d6F03E5e32F
+// Block: 22439516
+// Paid: 0.0615736875 C2FLR (985179 gas * 62.5 gwei)
 
 // ✅ Sequence #1 on flare-coston2 | Total Paid: 0.4659444375 C2FLR (7455111 gas * avg 62.5 gwei)
 
